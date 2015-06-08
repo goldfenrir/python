0f64fd39465f552cc757a954d19e0544be0a6bb8
goldfenrir <jdbustamante@pucp.pe>
2015-06-07 22:36:53 -0500

    Merge master into HEAD
    Conflicts:
    	Sistemas/src/sistemas/MantenerObjetivos.java

---
+++ src/minigText/Preprocesamiento.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "goldfenrir"
+__date__ = "$Jun 7, 2015 10:36:47 PM$"
+
+if __name__ == "__main__":
+    print "Hello World"
